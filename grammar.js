0e4376ce6f40507a1037d2c364e25a4b4e7d29d4
Thorsten Ball <mrnugget@gmail.com>
2022-06-18 10:31:52 +0200

    Add if-expressions and for loops

--- grammar.js
+++ grammar.js
@@ -4,105 +4,127 @@ module.exports = grammar({
   rules: {
     source_file: $ => repeat($._definition),
 
-    _definition: $ => choice(
-      $.function_definition
-      // TODO: other kinds of definitions
-    ),
+    _definition: $ =>
+      choice(
+        $.function_definition
+        // TODO: other kinds of definitions
+      ),
 
-    function_definition: $ => seq(
-      'funk',
-      field('name', $.identifier),
-      $.parameter_list,
-      optional($.return_type),
-      $.block
-    ),
+    function_definition: $ =>
+      seq(
+        'funk',
+        field('name', $.identifier),
+        $.parameter_list,
+        optional($.return_type),
+        $.block
+      ),
 
-    parameter_list: $ => seq(
-      '(',
-        optional(
-          seq(commaSep($.parameter), optional(','))
+    parameter_list: $ =>
+      seq('(', optional(seq(commaSep($.parameter), optional(','))), ')'),
+
+    parameter: $ =>
+      seq($.identifier, optional(field('type', seq(':', $.type)))),
+
+    return_type: $ => seq('->', $.type),
+
+    type: $ => choice('bool', 'string', 'int'),
+
+    block: $ => seq('{', repeat($._statement), '}'),
+
+    _statement: $ =>
+      seq(
+        choice(
+          $.expression_statement,
+          $.return_statement,
+          $.let_statement,
+          $.for_loop_statement
         ),
-      ')'
-    ),
+        optional(';')
+      ),
 
-    parameter: $ => seq(
-      $.identifier, optional(field('type', seq(':', $.type)))
-    ),
+    expression_statement: $ => $._expression,
 
-    return_type: $ => seq(
-      '->',
-      $.type,
-    ),
+    return_statement: $ => seq('return', $._expression),
 
-    type: $ => choice(
-      'bool',
-      'string',
-      'int'
-    ),
+    let_statement: $ =>
+      seq(
+        'let',
+        field('name', $.identifier),
+        optional(seq(':', $.type)),
+        field('operator', '='),
+        field('value', $._expression)
+      ),
 
-    block: $ => seq(
-      '{',
-      repeat($._statement),
-      '}'
-    ),
+    for_loop_statement: $ =>
+      seq(
+        'for',
+        '(',
+        optional(field('initializer', $.let_statement)),
+        ';',
+        optional(field('condition', $._expression)),
+        ';',
+        optional(field('increment', $._expression)),
+        ')',
+        field('body', $.block)
+      ),
 
-    _statement: $ => choice(
-      $.expression_statement,
-      $.return_statement,
-      $.let_statement
-      // TODO: other kinds of statements
-    ),
+    _expression: $ =>
+      choice(
+        $.if_expression,
+        $.true,
+        $.false,
+        $.identifier,
+        $.number,
+        $.assignment_expression,
+        $.binary_expression,
+        $.call_expression
+      ),
 
-    expression_statement: $ => seq(
-      $._expression,
-      ';'
-    ),
-
-    return_statement: $ => seq(
-      'return',
-      $._expression,
-      ';'
-    ),
-
-    let_statement: $ => seq(
-      'let',
-      field('name', $.identifier),
-      optional(seq(':', $.type)),
-      field('operator', '='),
-      field('value', $._expression),
-      ';'
-    ),
-
-    _expression: $ => choice(
-      $.true,
-      $.false,
-      $.identifier,
-      $.number,
-      $.assignment_expression,
-      $.binary_expression,
-      $.call_expression,
-    ),
-
-    binary_expression: $ => choice(
-      prec.left(4, seq(field('left', $._expression), field('operator', choice('-', '+')), field('right', $._expression))),
-      prec.left(3, seq(field('left', $._expression), field('operator', choice('>', '<', '=>', '<=', '==', '!=')), field('right', $._expression))),
-    ),
+    binary_expression: $ =>
+      choice(
+        prec.left(
+          4,
+          seq(
+            field('left', $._expression),
+            field('operator', choice('-', '+')),
+            field('right', $._expression)
+          )
+        ),
+        prec.left(
+          3,
+          seq(
+            field('left', $._expression),
+            field('operator', choice('>', '<', '=>', '<=', '==', '!=')),
+            field('right', $._expression)
+          )
+        )
+      ),
 
     assignment_expression: $ =>
-      prec.left(2, seq(field('left', $._expression), field('operator', '='), field('right', $._expression))),
+      prec.left(
+        2,
+        seq(
+          field('left', $._expression),
+          field('operator', '='),
+          field('right', $._expression)
+        )
+      ),
 
-    call_expression: $ => seq(
-      field('target', $.identifier),
-      field('args', $.argument_list),
-    ),
+    call_expression: $ =>
+      seq(field('target', $.identifier), field('args', $.argument_list)),
 
-    argument_list: $ => seq(
-      '(',
-        optional(
-          seq(commaSep($._expression), optional(','))
-        ),
-      ')'
-    ),
+    if_expression: $ =>
+      seq(
+        'if',
+        '(',
+        field('condition', $._expression),
+        ')',
+        field('consequence', $.block),
+        optional(seq('else', field('alternative', $.block)))
+      ),
+
+    argument_list: $ =>
+      seq('(', optional(seq(commaSep($._expression), optional(','))), ')'),
 
     _boolean_literal: $ => choice($.true, $.false),
 
@@ -111,14 +133,14 @@ module.exports = grammar({
 
     identifier: $ => /[a-z_]+/,
 
-    number: $ => /\d+/
-  }
+    number: $ => /\d+/,
+  },
 });
 
 function commaSep1(rule) {
-  return seq(rule, repeat(seq(',', rule)))
+  return seq(rule, repeat(seq(',', rule)));
 }
 
 function commaSep(rule) {
-  return optional(commaSep1(rule))
+  return optional(commaSep1(rule));
 }
